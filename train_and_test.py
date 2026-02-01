1fbc7b63ee7d2473638213817e0ef647bd94a647
ggasam0 <88809262+ggasam0@users.noreply.github.com>
2026-02-01 17:47:29 +0800

    Add MONK's dataset files and data loading functionality
    - Created new training and test datasets for MONK's problem 3.
    - Implemented data loading functions in load_data.py for MONK's datasets.
    - Updated load_data.py to handle loading of MONK's datasets alongside existing datasets (Iris and Congress).
    - Added a new script train_and_test.py to serve as the main entry point for training and testing classifiers.

---
+++ train_and_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import sys
+import load_data
+
+"""
+This is the main python method that will be run.
+You should determine what sort of command line arguments
+you want to use. But in this module you will need to 
+1) initialize your classifier and its params 
+2) load training/test data 
+3) train the algorithm
+4) test it and output the desired statistics.
+"""
